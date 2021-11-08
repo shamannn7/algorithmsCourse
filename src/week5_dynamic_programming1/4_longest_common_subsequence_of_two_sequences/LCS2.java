dbc8ebf57add5b624474c2306571add1da3103cb
Oleksiy Razoryonov <razoryonov@gmail.com>
2021-11-08 15:38:37 +0000

    dynamic programming - longest common subsequence

--- src/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/LCS2.java
+++ src/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/LCS2.java
@@ -4,7 +4,21 @@ public class LCS2 {
 
     private static int lcs2(int[] a, int[] b) {
         //Write your code here
-        return Math.min(a.length, b.length);
+        int[][] dp = new int[a.length + 1][b.length + 1];
+
+        // dynamic programming matrix
+        for (int i=0; i<a.length; i++){
+            for (int j=0;j<b.length; j++){
+                if(a[i] == b[j]){
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                }
+                else{
+                    dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
+                }
+            }
+        }
+
+        return dp[a.length][b.length];
     }
 
     public static void main(String[] args) {
