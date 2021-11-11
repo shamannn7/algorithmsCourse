70b37a03fcae272329d86d010d9b06ed03516952
Oleksiy Razoryonov <razoryonov@gmail.com>
2021-11-11 11:13:39 +0000

    dynamic programming - max amount of gold each item once

--- src/week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
+++ src/week6_dynamic_programming2/1_maximum_amount_of_gold/Knapsack.java
@@ -3,13 +3,36 @@ import java.util.*;
 public class Knapsack {
     static int optimalWeight(int W, int[] w) {
         //write you code here
-        int result = 0;
-        for (int i = 0; i < w.length; i++) {
-          if (result + w[i] <= W) {
-            result += w[i];
-          }
+        int [][] dp = new int[W + 1][w.length + 1];
+
+        for(int i=0; i <= W; i++){
+            dp[i][0] = 0;
         }
-        return result;
+        for(int i=0; i <= w.length; i++){
+            dp[0][i] = 0;
+        }
+
+        for(int i=1; i <= W; i++){
+            for(int j=0; j < w.length; j++){
+                
+                if(i-w[j] >= 0){
+                    dp[i][j+1] = Math.max(dp[i-w[j]][j] + w[j], dp[i][j]); // TODO we either take the element or not, second index i?
+                }
+                else{
+                    dp[i][j+1] = dp[i][j];
+                }
+
+//                System.out.println("dp["+i+"]["+j+"] = " + dp[i][j]);
+            }
+        }
+
+//        int result = 0;
+//        for (int i = 0; i < w.length; i++) {
+//          if (result + w[i] <= W) {
+//            result += w[i];
+//          }
+//        }
+        return dp[W][w.length];
     }
 
     public static void main(String[] args) {
