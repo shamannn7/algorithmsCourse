a396c5cd31068ec861c6bac7bf73cc042a0d42a8
Oleksiy Razoryonov <razoryonov@gmail.com>
2021-12-03 16:35:09 +0000

    Data Structures - Rabin Karp algo for finding pattern in text with hashing - unfunished

--- src/data_structures/week3_hash_tables/3_hash_substring/HashSubstring.java
+++ src/data_structures/week3_hash_tables/3_hash_substring/HashSubstring.java
@@ -30,21 +30,52 @@ public class HashSubstring {
     }
 
     private static List<Integer> getOccurrences(Data input) {
-        String s = input.pattern, t = input.text;
-        int m = s.length(), n = t.length();
+        String pattern = input.pattern, text = input.text;
+        int m = pattern.length(), n = text.length();
         List<Integer> occurrences = new ArrayList<Integer>();
+
+        long prime = ;// TODO big prime
+//        long phash = polyHash(x, prime);
+        long[] h = precomputeHashes(text, pattern, prime );
         for (int i = 0; i + m <= n; ++i) {
-	    boolean equal = true;
-	    for (int j = 0; j < m; ++j) {
-		if (s.charAt(j) != t.charAt(i + j)) {
-		     equal = false;
- 		    break;
-		}
-	    }
-            if (equal)
+            // TODO compare precomupte hash and continue if they don't match
+            if(pHash != h[i])
+                continue;
+
+            if (areEqual(pattern, text, m, i))
                 occurrences.add(i);
-	}
+	    }
         return occurrences;
+    }
+
+    private static long polyHash(long x, long prime) {
+        return (32*x + 2) % prime;
+    }
+
+    private static long[] precomputeHashes(String text, String pattern, int prime){
+        long[] result = new long[text.length() - pattern.length() + 1];
+            // TODO  what is x here??
+        String s = text.substring(text.length() - pattern.length());
+        int y =1;
+        for (int i = 1; i < pattern.length(); i++) {
+            y = (y*x) % prime;
+        }
+        for (int i = text.length()-pattern.length()-1; i >=0 ; i--) {
+            result[i] = (x*result[i+1] + text.charAt(i) - y * text.charAt(i+pattern.length())) % prime;
+        }
+        
+        return result;
+    }
+
+    private static boolean areEqual(String s, String t, int m, int i) {
+        boolean equal = true;
+        for (int j = 0; j < m; ++j) {
+        if (s.charAt(j) != t.charAt(i + j)) {
+             equal = false;
+            break;
+        }
+        }
+        return equal;
     }
 
     static class Data {
